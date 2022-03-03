b1a1582cd17bec7138178af65ed38af18b1439c2
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-03 18:31:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/TechShreyash/SiestaXMusic"
+UPSTREAM_REPO = "https://github.com/AMANTYA1/l-baji"
 UPSTREAM_BRANCH = "main"
 
-SUPPORT_CHANNEL = "https://t.me/TechZBots"
-SUPPORT_GROUP = "https://t.me/TechZBots_Support"
+SUPPORT_CHANNEL = "https://t.me/kigo_omfo"
+SUPPORT_GROUP = "https://t.me/OmFoXD"
 
 THUMBNAIL = getenv("THUMB_LINK") 
 
